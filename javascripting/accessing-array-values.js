1601b023704410de55b21047eb911c96fa208fc5
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-16 23:49:35 +1100

    first commit

---
+++ javascripting/accessing-array-values.js
@@ -0,0 +1,5 @@
+//print the second value of array to the terminal.
+
+const food = ['apple', 'pizza', 'pear'];
+
+console.log(food[1]);
---
+++ javascripting/function-arguments.js
@@ -0,0 +1,7 @@
+function math(a, b, c) {
+    return (b * c) + a;
+}
+
+// math((a, b, c) => (b * c) + a);
+
+console.log(math(53, 61, 67));
