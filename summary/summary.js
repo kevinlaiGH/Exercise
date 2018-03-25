0640a0987fb0c00eaad6556a9bd85f87150a234f
Kevin Lai <kevin.km.lai@gmail.com>
2018-03-25 14:12:11 +1100

    added summary

---
+++ summary/summary.js
@@ -0,0 +1,17 @@
+// 1. Push Method: add items to the end of an array, returns to new length
+
+let fruits = ['🍌', '🍊', '🍎', '🍋'];
+
+fruits.push['🍇'] // 🍌, 🍊, 🍎, 🍋, 🍇
+
+// 2. Pop() Method:  removes last element of an array and returns that element
+
+let fruits = ['🍌', '🍊', '🍎', '🍋'];
+
+fruits.pop() // 🍌, 🍊, 🍎
+
+// 3. Find Method: find an element in an array
+
+
+
+
