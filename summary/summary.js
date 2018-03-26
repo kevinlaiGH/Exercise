76d1672f11cf892d5cfd3a68d40527a99f36e829
Kevin Lai <kevin.km.lai@gmail.com>
2018-03-26 20:55:34 +1100

    Added methods

--- summary/summary.js
+++ summary/summary.js
@@ -12,6 +12,27 @@ fruits.pop() // 🍌, 🍊, 🍎
 
 // 3. Find Method: find an element in an array
 
+function BiggerThan(element) {
+    return element >= 15;
+}
+
+[12, 5, 8, 130, 44].find(BiggerThan); //130
+
+//4. Filter Method: Filters an array based on callback returning true or false.
+// If callback returns true it willbe included in the result
+
+var name = ['Kevin', 'Ka Ming', 'Lai'];
+const output = names.filter(t => t.length > 3);
+console.log(output);
+// output['Kevin', 'Ka Ming']
+
+// 5. Sort Method: The Sort Method sorts the items of an array.
+// By default, the sort() method sorts the values as strings in alphabetical order and ascending order.
+
+let fruits = ['Banana', 'Orange', 'Apple', "Mango"];
+fruits.sort();
+// output = Apple, Banana, Mango, Orange
+
 
 
 
