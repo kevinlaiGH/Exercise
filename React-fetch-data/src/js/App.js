8f5bffb053ce7639731965d8401298b979f85641
Kevin Lai <kevin.km.lai@gmail.com>
2018-05-31 14:27:47 +1000

    create intial loading state

--- React-fetch-data/src/js/App.js
+++ React-fetch-data/src/js/App.js
@@ -1,30 +1,65 @@
-import React from 'react';
-import image from '../images/cloud-upload-download-data-transfer.svg';
-import Collapsible from './Collapsible';
+import React from "react";
+import image from "../images/cloud-upload-download-data-transfer.svg";
+import Collapsible from "./Collapsible";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <img src={image} />
-                    <h1>Fetching Data <button className="btn btn-sm btn-danger">Fetch now</button></h1>
-                </header>
-                <div className="content">
-                    <div className="panel-group">
-                        <Collapsible title="Tracy Palmer">
-                            <p>tracey.palmer@example.com<br />3280 manchester road, ely</p>
-                        </Collapsible>
-                        <Collapsible title="Jade Dupuis">
-                            <p>jade.dupuis@example.com<br />1803 rue pasteur, nanterre</p>
-                        </Collapsible>
-                        <Collapsible title="Alfred Olsen">
-                            <p>alfred.olsen@example.com<br />6598 fjordparken, hirtsals</p>
-                        </Collapsible>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      contacts: []
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  // fetchData()
+  render() {
+    const { isLoading, contacts } = this.state;
+    return (
+      <div>
+        <header>
+          <img src={image} />
+          <h1>
+            Fetching Data{" "}
+            <button className="btn btn-sm btn-danger">Fetch now</button>
+          </h1>
+        </header>
+        <div className={`content ${isLoading ? "is-loading" : ""}`}>
+          <div className="panel-group">
+            {!isLoading && contacts.length > 0
+              ? contacts.map(contact => {
+                  return (
+                    <Collapsible title="Tracy Palmer">
+                      <p>
+                        tracey.palmer@example.com<br />3280 manchester road, ely
+                      </p>
+                    </Collapsible>
+                  );
+                })
+              : null}
+            <Collapsible title="Tracy Palmer">
+              <p>
+                tracey.palmer@example.com<br />3280 manchester road, ely
+              </p>
+            </Collapsible>
+            <Collapsible title="Jade Dupuis">
+              <p>
+                jade.dupuis@example.com<br />1803 rue pasteur, nanterre
+              </p>
+            </Collapsible>
+            <Collapsible title="Alfred Olsen">
+              <p>
+                alfred.olsen@example.com<br />6598 fjordparken, hirtsals
+              </p>
+            </Collapsible>
+          </div>
+          <div className="loader">
+            <div className="icon" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
